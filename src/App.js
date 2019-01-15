ec415c4566364450b71dcf16b3801792f5041d7b
jungRoit <roit.jung@gmail.com>
2019-01-15 23:01:54 +0545

    eslint config added

--- src/App.js
+++ src/App.js
@@ -4,133 +4,207 @@ import Form from './components/form';
 import Header from './components/header';
 import Container from './components/container';
 
+/** 
+ * .App class includes the main app logic and handles all the functions including add, edit ,delete ,search sort.
+ *  
+ */
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			todos: JSON.parse(localStorage.getItem('todos')) || [],
-			searchList: [],
-			sortedList: [],
-			isSearchEnabled: false,
-			isSortEnabled: false
-		}
-	}
+  /** 
+	 * 
+	 * @param {*} props 
+	 */
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: JSON.parse(localStorage.getItem('todos')) || [],
+      searchList: [],
+      sortedList: [],
+      isSearchEnabled: false,
+      isSortEnabled: false
+    }
+  }
 
-	setLocalStorage = () => {
-		window.addEventListener('beforeunload', () => {
-			localStorage.clear();
-			localStorage.setItem('todos', JSON.stringify(this.state.todos) || null);
-		})
-	}
+  /**
+	 * .Store todo item in local storage.
+	 */
+  setLocalStorage = () => {
+    window.addEventListener('beforeunload', () => {
+      localStorage.clear();
+      localStorage.setItem('todos', JSON.stringify(this.state.todos) || null);
+    });
+  }
 
-	componentDidMount() {
-		this.setLocalStorage();
-	}
+  /**
+	 * 
+	 */
+  componentDidMount() {
+    this.setLocalStorage();
+  }
 
-	addTodo = (todo) => {
-		if (todo.value !== '') {
-			this.setState({
-				todos: this.state.todos.concat(todo)
-			})
-		}
-	}
+  /**
+	 * .Function to add new todo item.
+   * 
+   * @param {*} todo
+	 */
+  addTodo = (todo) => {
+    if (todo.value !== '') {
+      this.setState({
+        todos: this.state.todos.concat(todo)
+      });
+    }
+  }
 
-	search = (val) => {
-		let result = this.state.todos.filter(item => item.value.includes(val));
-		this.setState({ searchList: result });
-	}
+  /**
+ * .Function to search todo item.
+ * 
+ * @param {*} val
+ */
+  search = (val) => {
+    const result = this.state.todos.filter(item => item.value.includes(val));
 
-	deleteTodo = (todo) => {
-			let result = this.state.todos.filter(item => item !== todo);
-			this.setState({todos:result});
-	}
+    this.setState({ searchList: result });
+  }
 
-	// Header Methods for toggle between input bar
-	toggleSearch = () => {
-		if (this.state.isSearchEnabled) this.setState({ isSearchEnabled: false });
-		else this.setState({ isSearchEnabled: true });
-	}
+  /**
+ * .Function to delete todo item.
+ * 
+ * @param {*} todo
+ */
+  deleteTodo = (todo) => {
+    const result = this.state.todos.filter(item => item !== todo);
 
-	sortTodos = (value) => {
-		let result = [];
-		if (value === '1') {
-			result = this.state.todos.filter(item => item.isComplete === true);
-			this.setState({  sortedList: result, isSortEnabled:true });
-		}else if(value === '2') {
-			result = this.state.todos.filter(item => item.isComplete === false);
-			this.setState({ sortedList: result, isSortEnabled:true });
-		}else {
-			this.setState({isSortEnabled:false});
-		}
-	}
+    this.setState({ todos: result });
+  }
 
-	toggleTaskCompleted = (todo) => {
-		let todos = this.state.todos.slice();
-		if (todo.isComplete) todo.isComplete = false;
-		else todo.isComplete = true;
-		let index = todos.indexOf(todo);
-		todos[index] = todo;
-		this.setState({ todos })
-	}
+  /**
+ * 
+ */
+  toggleSearch = () => {
+    if (this.state.isSearchEnabled) {
+      this.setState({ isSearchEnabled: false });
+    } else {
+      this.setState({ isSearchEnabled: true });
+    }
+  }
 
-	toggleIsEditEnabled = (todo) => {
-		let todos = this.state.todos.slice();
-		if (todo.isEditEnabled) todo.isEditEnabled = false;
-		else todo.isEditEnabled = true;
-		let index = todos.indexOf(todo);
-		todos[index] = todo;
-		this.setState({ todos })
-	}
+  /**
+ * .function to sort todoList.
+ * 
+ * @param {*} value
+ */
+  sortTodos = (value) => {
+    let result = [];
 
+    if (value === '1') {
+      result = this.state.todos.filter(item => item.isComplete === true);
+      this.setState({ sortedList: result, isSortEnabled: true });
+    } else if (value === '2') {
+      result = this.state.todos.filter(item => item.isComplete === false);
+      this.setState({ sortedList: result, isSortEnabled: true });
+    } else {
+      this.setState({ isSortEnabled: false });
+    }
+  }
 
+  /**
+ * .Function to toggle isComplete variable of todo item.
+ * 
+ * @param {*} todo
+ */
+  toggleTaskCompleted = (todo) => {
+    const todos = this.state.todos.slice();
 
-	editTodo = (id, value) => {
-		let newTodos = this.state.todos.filter((item) => {
-			if (item.id === id) item.value = value;
-			return item;
-		})
-		this.setState({ todos: newTodos })
-	}
+    if (todo.isComplete) {
+      todo.isComplete = false;
+    } else {
+      todo.isComplete = true;
+    }
+    const index = todos.indexOf(todo);
 
-	render() {
-		return (
-			<div className="App" >
-				<Header
-					title="To-do App"
-					toggleSearch={this.toggleSearch}
-					sortTodos={this.sortTodos}
-				/>
+    todos[index] = todo;
+    this.setState({ todos })
+  }
 
-				{this.state.isSearchEnabled
-					? (
-						<Form
-							canSearch={true}
-							clicked={this.search}
-							placeHolder='Search a To-do'
-						/>
-					)
-					:
-					(
-						<Form
-							canSearch={false}
-							clicked={this.addTodo}
-							placeHolder='Create a new To-do'
-						/>
-					)
-				}
+  /**
+ * Function to toggle isEditEnabled to toggle between paragraph and input.
+ * 
+ * @param {*} todo
+ */
+  toggleIsEditEnabled = (todo) => {
+    const todos = this.state.todos.slice();
 
-				<Container
-					todos= {this.state.isSearchEnabled ? this.state.searchList : (this.state.isSortEnabled 
-					? this.state.sortedList: this.state.todos)}
-					deleteHandler={this.deleteTodo}
-					toggleTaskCompleted={this.toggleTaskCompleted}
-					toggleIsEditEnabled={this.toggleIsEditEnabled}
-					editTodo={this.editTodo}
-				/>
+    if (todo.isEditEnabled) {
+      todo.isEditEnabled = false;
+    } else {
+      todo.isEditEnabled = true;
+    }
+    const index = todos.indexOf(todo);
 
-			</div>
-		);
-	}
+    todos[index] = todo;
+    this.setState({ todos })
+  }
+
+  /**
+ * .Function to edit todo item.
+ * 
+ * @param {*} id
+ * 
+ * @param {*} value
+ */
+  editTodo = (id, value) => {
+    const newTodos = this.state.todos.filter((item) => {
+      if (item.id === id) {
+        item.value = value;
+      }
+
+      return item;
+    });
+
+    this.setState({ todos: newTodos })
+  }
+
+  /**
+ * .Function to render jsx.
+ */
+  render() {
+    return (
+      <div className="App" >
+        <Header
+          title="To-do App"
+          toggleSearch={this.toggleSearch}
+          sortTodos={this.sortTodos}
+        />
+
+        {this.state.isSearchEnabled
+          ? (
+            <Form
+              canSearch={true}
+              clicked={this.search}
+              placeHolder='Search a To-do'
+            />
+          )
+          :
+          (
+            <Form
+              canSearch={false}
+              clicked={this.addTodo}
+              placeHolder='Create a new To-do'
+            />
+          )
+        }
+
+        <Container
+          todos={this.state.isSearchEnabled ? this.state.searchList : (this.state.isSortEnabled
+            ? this.state.sortedList : this.state.todos)}
+          deleteHandler={this.deleteTodo}
+          toggleTaskCompleted={this.toggleTaskCompleted}
+          toggleIsEditEnabled={this.toggleIsEditEnabled}
+          editTodo={this.editTodo}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,47 +1,69 @@
 import React from 'react';
 import './index.css';
 
+/**
+ * .Form Component to create an input.
+ */
 class Form extends React.Component {
+/**
+ * 
+ * @param {*} props 
+ */
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: '',
-		}
-	} 
+  /**
+   * .Function to set value of input field when changed to state.
+   * 
+   * @param {*} event
+   */
+  getValue = (event) => {
+    this.setState({ value: event.target.value });
+    if (this.props.canSearch) {
+      this.props.clicked(event.target.value);
+    }
+  }
 
-	getValue = (event) => {
-		this.setState({value:event.target.value});
-		if(this.props.canSearch) this.props.clicked(event.target.value);
-	}
+  /**
+   * .Function to send todo object to parent.
+   * 
+   * @param {*} event
+   */
+  getInputValue = (event) => {
+    if (event.keyCode === 13) {
+      const note = {
+        id: new Date().toISOString(),
+        value: this.state.value,
+        isComplete: false,
+        isEditEnabled: false,
+        isChecked: false
+      };
 
-	getInputValue = (event) => {
-		if(event.keyCode === 13){
-			let note = {
-				id: new Date().toISOString(),
-				value:this.state.value,
-				isComplete: false,
-				isEditEnabled:false,
-				isChecked:false
-			}
-			this.props.clicked(note);
-			this.setState({value:''});
-		}
-		
-	}
+      this.props.clicked(note);
+      this.setState({ value: '' });
+    }
 
-	render() {
-		return (
-			<div className="form">
-			<input 
-			className= "input"
-			value={this.state.value} onChange={this.getValue}
-			placeholder= {this.props.placeHolder}
-			onKeyDown = {this.getInputValue}
-			/>
-			</div>
-		)
-	}
+  }
+
+  /**
+   * Render method for Form Component's jsx.
+   */
+  render() {
+    return (
+      <div className="form">
+        <input
+          className="input"
+          value={this.state.value} onChange={this.getValue}
+          placeholder={this.props.placeHolder}
+          onKeyDown={this.getInputValue}
+        />
+      </div>
+    );
+  }
 }
 
 export default Form;
